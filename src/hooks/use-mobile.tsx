28eb2ca55ea3c02747b3a23de703a309ba939313
[dyad] <git@dyad.sh>
2025-08-26 04:00:00 +0530

    [dyad] Fixing mobile blank screen bug - wrote 1 file(s)

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -3,19 +3,24 @@ import * as React from "react";
 const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
-    undefined,
-  );
+  const [isMobile, setIsMobile] = React.useState(false);
 
   React.useEffect(() => {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+
+    // Set the initial value correctly
+    setIsMobile(mql.matches);
+
+    const handleMediaChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
     };
-    mql.addEventListener("change", onChange);
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener("change", onChange);
+
+    mql.addEventListener("change", handleMediaChange);
+
+    return () => {
+      mql.removeEventListener("change", handleMediaChange);
+    };
   }, []);
 
-  return !!isMobile;
+  return isMobile;
 }
