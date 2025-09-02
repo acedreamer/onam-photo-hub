a8646b33437acba4b15176b902194f807f8b9a63
acedreamer <abishek4705@gmail.com>
2025-09-02 11:44:47 +0530

    Fixing Supabase function deployment error - wrote 4 file(s)

--- supabase/functions/cloudinary-upload/index.ts
+++ supabase/functions/cloudinary-upload/index.ts
@@ -1,5 +1,5 @@
-import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
-import { createHash } from "https://deno.land/std@0.177.0/node/crypto.ts";
+import { serve } from "https://deno.land/std@0.208.0/http/server.ts";
+import { createHash } from "https://deno.land/std@0.208.0/node/crypto.ts";
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
